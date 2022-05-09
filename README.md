d1c1355d24b9021dd1201620f059e7516f1e5d14
Soussi-Aymen <aymensoussi.02@gmail.com>
2022-05-09 16:14:02 +0100

    javascript syntaxe improved

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Socket.io-simple-chat-app
-This repository contains a simple chat application made with Socket.io and Node.js
+This repository contains a simple chat web application made with Socket.io library, Node.js and Bootstrap.
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express =require('express');
-var app=express();
-var server=require("http").createServer(app);
-var io=require('socket.io').listen(server);
+let express =require('express');
+let app=express();
+let server=require("http").createServer(app);
+let io=require('socket.io').listen(server);
 users=[];
 connections= [];
 server.listen(process.env.PORT||3000);
 console.log('server runing');
 
-app.get('/',function(req,res){
+app.get('/',function(request,response){
 res.sendFile(__dirname+'/index.html');
 });
 
